0e30b18478180b95881a3239690c889d68e228e7
jackabnana <jackabnana@gmail.com>
2021-07-08 09:54:47 +0545

    made dynamic

--- online/blog/views.py
+++ online/blog/views.py
@@ -1,5 +1,14 @@
+from blog.models import Post
+from typing import ClassVar
+from django.db import models
 from django.shortcuts import render
-
+from django.views.generic import ListView, DetailView
+from .models import Post
 # Create your views here.
+
 def index (request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {})
+
+class BlogView(ListView):
+    model = Post
+    template_name = 'blog.html'
--- online/online/urls.py
+++ online/online/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from blog.views import index
+from django.urls import path, include
+from blog.views import index, BlogView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index)
+    path('', include('blog.urls')),
+    path('blog/', BlogView.as_view(), name="blog" ),
 ]
