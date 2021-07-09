c1dc2e7038b73f33b3f3c8a789cde68c365d9a20
jackabnana <jackabnana@gmail.com>
2021-07-09 17:54:55 +0545

    category made

--- online/blog/models.py
+++ online/blog/models.py
@@ -3,12 +3,24 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 from datetime import datetime, date
 
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+    def get_absolute_url(self):
+        #return reverse('article-detail', args=(str(self.id)))
+        #return reverse('article-detail', args=(str(self.id))),
+        return reverse('blog')
+
+
 class Post(models.Model):
     title = models.CharField(max_length=255)
     title_tag = models.CharField(max_length=255,default="tradebulletin")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
     post_date = models.DateField(auto_now_add='True')
+    category = models.CharField(max_length=255, default='uncategorised')
     #image = models.ImageField(upload_to="media/", blank=True)
 
     def __str__(self):
@@ -18,3 +30,4 @@ class Post(models.Model):
         #return reverse('article-detail', args=(str(self.id)))
         #return reverse('article-detail', args=(str(self.id))),
         return reverse('blog')
+
--- online/blog/views.py
+++ online/blog/views.py
@@ -3,9 +3,10 @@ from typing import ClassVar
 from django import forms
 from django.db import models
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
-from .models import Post
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Category, Post
 from .forms import PostForm, EditForm
+from django.urls import reverse_lazy
 # Create your views here.
 
 def index (request):
@@ -13,6 +14,10 @@ def index (request):
 
 def about (request):
     return render(request, 'about.html', {})
+
+def CategoryView (request, cats):
+    category_posts = Post.objects.filter(category=cats),
+    return render(request, 'categories.html', {'cats':cats, 'category_posts':category_posts})
 
 class BlogView(ListView):
     model = Post
@@ -29,9 +34,21 @@ class AddPostView(CreateView):
     form_class = PostForm
     template_name = 'add_post.html'
     #fields = '__all__'
+
+class AddCategoryView(CreateView):
+    model = Category
+    #form_class = PostForm
+    template_name = 'add_category.html'
+    fields = '__all__'
     
 class UpdatePostView(UpdateView):
     model = Post
     form_class = EditForm
     template_name = 'update.html'
     #fields = ['title', 'title_tag', 'body']
+
+class DeletePostView(DeleteView):
+    model = Post
+    template_name = 'delete_post.html'
+    success_url = reverse_lazy('blog')
+   
