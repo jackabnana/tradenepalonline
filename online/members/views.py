11794ff6c0d1f1ed4a78ceadd0a1c065e41dd543
jackabnana <jackabnana@gmail.com>
2021-07-12 06:47:36 +0545

    commited

--- online/members/views.py
+++ online/members/views.py
@@ -1,18 +1,41 @@
 from django import forms
 from django.contrib.auth.views import PasswordChangeView
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.views import generic
 from django.contrib.auth.forms import PasswordChangeForm, UserCreationForm, UserChangeForm
 from django.urls import reverse_lazy
+from django.views.generic.list import ListView
 from .forms import SignUpForm, EditProfileForm, PasswordChangingForm
-from django.views.generic import DetailView
+from django.views.generic import DetailView, CreateView
 from blog.models import Profile
+
+class CreateProfilePageView(CreateView):
+    model = Profile
+    template_name = "registration/create_user_profile_page.html"
+    fields = '__all__'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+class EditProfilePageView(generic.UpdateView):
+    model = Profile
+    template_name = 'registration/edit_profile_page.html'
+    fields = [ 'bio', 'profile_pic', 'Website_url', 'facebook_url', 'twitter_url', 'instagram_url']
+    success_url = reverse_lazy('home')
 
 
 
 class ShowProfilePageView(DetailView):
     model = Profile
     template_name = 'registration/user_profile.html'
+
+    def get_context_data(self, *args, **kwargs):
+        users = Profile.objects.all()
+        context = super(ShowProfilePageView, self).get_context_data(*args, **kwargs)
+        page_user = get_object_or_404(Profile, id=self.kwargs['pk'])
+        context["page_user"] = page_user
+        return context
 
 class PasswordsChangeView(PasswordChangeView):
     form_class = PasswordChangingForm
